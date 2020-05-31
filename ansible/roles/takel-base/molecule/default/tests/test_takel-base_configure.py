5093cb2fe5862be8e8b0b3f9654b8df867575052
Stephan Wehrheim <swehrheim@geospin.de>
2020-06-01 00:23:29 +0200

    Add takel-base

---
+++ ansible/roles/takel-base/molecule/default/tests/test_takel-base_configure.py
@@ -0,0 +1,17 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_base_configure_locales(host, testvars):
+    file = host.file('/etc/default/locale')
+    locales = testvars['takel_base_locales']
+
+    assert file.exists
+    assert file.is_file
+    assert file.user == 'root'
+    assert file.group == 'root'
+    assert file.mode == 0o644
+
+    for locale in locales:
+        assert locale in file.content_string
