5f3ffd30f94c72e0ad573dfa36dda1338dba0f1c
Stephan Wehrheim <swehrheim@geospin.de>
2020-06-28 17:10:54 +0200

    Fix virtualbox vagrant target

--- packer/templates/vbox/base/debian10/bin/sudo.sh
+++ packer/templates/vbox/base/debian10/bin/sudo.sh
@@ -7,9 +7,3 @@ grep -q 'secure_path' /etc/sudoers \
 # Set up password-less sudo for the vagrant user
 echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;
 chmod 440 /etc/sudoers.d/99_vagrant;
-
-# Add shared ssh key which will be replaced during vagrant up
-mkdir -pm 700 /home/vagrant/.ssh
-curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
-chmod 0600 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
