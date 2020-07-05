d93f844ef47f5509780c33bea908dca4940975cc
Stephan Wehrheim <swehrheim@geospin.de>
2020-07-05 18:34:28 +0200

    tau paste takelage.ansible/ansible/roles/takel-systemd ansible/roles/takel-systemd

---
+++ ansible/roles/takel-systemd/molecule/default/system/test_takel-systemd_system_services.py
@@ -0,0 +1,17 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_systemd_services_service_enabled(host, testvars):
+    systemd_services_list = testvars['takel_systemd_service_list']
+    systemd_service_enabled = \
+        host.check_output('systemctl list-unit-files | grep enabled')
+    for service in systemd_services_list:
+        assert service['name'] in systemd_service_enabled
+
+
+def test_takel_systemd_services_service_running(host, testvars):
+    systemd_services_list = testvars['takel_systemd_service_list']
+    for service in systemd_services_list:
+        assert host.service(service['name']).is_running
---
+++ ansible/roles/takel-systemd/molecule/default/tests/test_takel-systemd_services.py
@@ -0,0 +1,13 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_systemd_services_files_exists(host, testvars):
+    systemd_services_list = testvars['takel_systemd_service_list']
+    for service in systemd_services_list:
+        expected_path = f"/etc/systemd/system/{service['name']}.service"
+        assert host.file(expected_path).is_file
+        assert host.file(expected_path).user == 'root'
+        assert host.file(expected_path).group == 'root'
+        assert host.file(expected_path).mode == 0o0644
