61a43bdbb4d0ab1b39d7fed7ecd690f0b3304b3b
Stephan Wehrheim <swehrheim@geospin.de>
2020-08-02 18:45:49 +0200

    Remove define from Vagrantfile

--- packer/templates/vbox/takelbase/project/vagrantfile/takelpad.rb
+++ packer/templates/vbox/takelbase/project/vagrantfile/takelpad.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Vagrant.configure(2) do |config|
-  config.vm.define :'takelpad'
   config.vm.hostname = 'takelpad'
   config.vm.network :public_network
   config.vm.provision 'shell', run: 'always', inline: <<-SHELL
