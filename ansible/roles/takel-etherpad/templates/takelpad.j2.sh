d84513121f1388dff5afbeb08a93dc5be9c45e7a
Stephan Wehrheim <swehrheim@geospin.de>
2020-08-03 02:49:33 +0200

    Fix variable names

--- ansible/roles/takel-etherpad/templates/takelpad.j2.sh
+++ ansible/roles/takel-etherpad/templates/takelpad.j2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION_FILE={{ takel_takelpad_version_file }}
+VERSION_FILE={{ takel_etherpad_version_file }}
 if test -f "$VERSION_FILE"; then
   VERSION=$(head -n 1 "$VERSION_FILE")
   HEADER="takelpad: $VERSION"
@@ -13,9 +13,9 @@ echo "takelpad ip address: $IP_ETHERPAD"
 
 if [ "$1" != "--summary" ]; then
   IP_ADDRESSES=$(ip --json address | jq -r '.[].addr_info[0].local')
-  ETHERPAD_ADMIN_PASSWORD=$(sudo {{ takel_takelpad_bin }}/etherpad-admin-password-get)
-  MYSQL_ETHERPAD_USER=$(sudo grep user {{ takel_takelpad_home }}/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
-  MYSQL_ETHERPAD_PASSWORD=$(sudo grep password {{ takel_takelpad_home }}/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
+  ETHERPAD_ADMIN_PASSWORD=$(sudo {{ takel_etherpad_bin }}/etherpad-admin-password-get)
+  MYSQL_ETHERPAD_USER=$(sudo grep user {{ takel_etherpad_home }}/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
+  MYSQL_ETHERPAD_PASSWORD=$(sudo grep password {{ takel_etherpad_home }}/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
   MYSQL_ROOT_USER=$(sudo grep user /root/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
   MYSQL_ROOT_PASSWORD=$(sudo grep password /root/.my.cnf | sed -e 's/.*"\([^"]*\)"/\1/')
 
@@ -32,9 +32,10 @@ if [ "$1" != "--summary" ]; then
   echo "mysql root user: $MYSQL_ROOT_USER"
   echo "mysql root password: $MYSQL_ROOT_PASSWORD"
   echo
-  echo "start: vagrant up"
-  echo "stop: vagrant halt"
-  echo "destroy: vagrant destroy"
+  echo "start box: vagrant up"
+  echo "stop box: vagrant halt"
+  echo "destroy box: vagrant destroy"
+  echo
   echo "login user: vagrant ssh"
   echo "login root: vagrant ssh -c 'sudo su -'"
 fi
