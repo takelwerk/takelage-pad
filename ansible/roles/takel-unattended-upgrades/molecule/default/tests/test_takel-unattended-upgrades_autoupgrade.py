c13e1191d07e3583cd259b2058571f539ab0417f
Stephan Wehrheim <swehrheim@geospin.de>
2020-08-10 12:52:53 +0200

    tau paste takel-unattended-upgrades

---
+++ ansible/roles/takel-unattended-upgrades/molecule/default/tests/test_takel-unattended-upgrades_autoupgrade.py
@@ -0,0 +1,33 @@
+
+
+def test_takel_unattended_upgrades_autoupgrade_20auto_upgrades(host, testvars):
+    autoupgrade = testvars['takel_unattended_upgrades_autoupgrade']
+    if autoupgrade:
+        file = host.file('/etc/apt/apt.conf.d/20auto-upgrades')
+
+        assert file.exists
+        assert file.is_file
+        assert file.user == 'root'
+        assert file.group == 'root'
+        assert file.mode == 0o644
+
+
+def test_takel_unattended_upgrades_autoupgrade_50unattended_upgrades(host,
+                                                                     testvars):
+    autoupgrade = testvars['takel_unattended_upgrades_autoupgrade']
+    if autoupgrade:
+        file = host.file('/etc/apt/apt.conf.d/50unattended-upgrades')
+
+        assert file.exists
+        assert file.is_file
+        assert file.user == 'root'
+        assert file.group == 'root'
+        assert file.mode == 0o644
+
+
+def test_takel_unattended_upgrades_autoupgrade__installed(host, testvars):
+    autoupgrade = testvars['takel_unattended_upgrades_autoupgrade']
+    if autoupgrade:
+        package = host.package('unattended-upgrades')
+
+        assert package.is_installed
---
+++ ansible/roles/takel-unattended-upgrades/molecule/default/tests/test_takel-unattended-upgrades_configure.py
@@ -0,0 +1,28 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_unattended_upgrades_autoupgrade_20auto_upgrades(host, testvars):
+    autoupgrade = testvars['takel_unattended_upgrades_autoupgrade']
+    if autoupgrade:
+        file = host.file('/etc/apt/apt.conf.d/20auto-upgrades')
+
+        assert file.exists
+        assert file.is_file
+        assert file.user == 'root'
+        assert file.group == 'root'
+        assert file.mode == 0o644
+
+
+def test_takel_unattended_upgrades_autoupgrade_50unattended_upgrades(host,
+                                                                     testvars):
+    autoupgrade = testvars['takel_unattended_upgrades_autoupgrade']
+    if autoupgrade:
+        file = host.file('/etc/apt/apt.conf.d/50unattended-upgrades')
+
+        assert file.exists
+        assert file.is_file
+        assert file.user == 'root'
+        assert file.group == 'root'
+        assert file.mode == 0o644
