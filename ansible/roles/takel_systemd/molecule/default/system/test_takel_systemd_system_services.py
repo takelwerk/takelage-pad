6d2867fcdc988bb9ec670573b93eee692a418d8d
Stephan Wehrheim <swehrheim@geospin.de>
2021-10-12 18:18:58 +0200

    Refactor takel_systemd

--- ansible/roles/takel_systemd/molecule/default/system/test_takel_systemd_system_services.py
+++ ansible/roles/takel_systemd/molecule/default/system/test_takel_systemd_system_services.py
@@ -11,15 +11,11 @@ def test_takel_systemd_services_service_enabled(host, testvars):
         assert service['name'] in systemd_service_enabled
 
 
-# def test_takel_systemd_services_service_running(host, testvars):
-#     systemd_services_list = testvars['takel_systemd_service_list']
-#     for service in systemd_services_list:
-#         assert host.service(service['name'] + '.service').is_running
-
-
 def test_takel_systemd_services_service_running(host, testvars):
-    systemd_services_list = testvars['takel_systemd_service_list']
-    systemd_service_enabled = \
-        host.check_output('systemctl list-unit-files | grep enabled')
-    for service in systemd_services_list:
-        assert service['name'] in systemd_service_enabled
+    assert host.service('example-service.service').is_running
+
+
+def test_takel_systemd_services_service_triggered(host, testvars):
+    log = host.check_output('journalctl -u example-service-timer')
+    print(log)
+    assert 'example-service-timer.service: Succeeded.' in log
