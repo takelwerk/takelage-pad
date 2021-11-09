a9a75099f04d9b9f2f03d254fb632964dc9200ec
Stephan Wehrheim <swehrheim@geospin.de>
2021-11-09 21:45:29 +0100

    Add packer parallels template

---
+++ packer/templates/takelage/parallels/bin/ssh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -eux
+
+# Add shared ssh key which will be replaced during vagrant up
+mkdir -pm 700 /home/vagrant/.ssh
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
+chmod 0600 /home/vagrant/.ssh/authorized_keys
+chown -R vagrant:vagrant /home/vagrant/.ssh
