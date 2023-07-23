b4757adac74e295528cadd19448d5ab416e5f585
Stephan Wehrheim <swehrheim@geospin.de>
2023-07-23 14:22:40 +0200

    hg recover

--- ansible/roles/takel_anarchism/molecule/default/system/test_takel_anarchism_system.py
+++ ansible/roles/takel_anarchism/molecule/default/system/test_takel_anarchism_system.py
@@ -7,7 +7,7 @@ def test_takel_anarchism_system_fortune_greeting(host, testvars):
     if 'fortune-anarchism' in testvars['takel_anarchism_deb_install_packages']:
         with host.sudo():
             output = host.check_output('bash -c ". /root/.bashrc"')
-            assert '---+-' in output
+            assert '-- ' in output
 
 
 def test_takel_root_system_includes_alias(host, testvars):
