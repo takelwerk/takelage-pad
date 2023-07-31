b21b8ade7495590250f493e31611e544b7c8fbb2
Stephan <stephan@takelwerk.net>
2023-07-31 18:14:24 +0200

    Refactor docker image test

--- ansible/molecule/image/tests/conftest.py
+++ ansible/molecule/image/tests/conftest.py
@@ -7,16 +7,16 @@ image_tag = 'testing'
 
 
 @pytest.fixture(scope='module')
-def docker_api():
-    docker_host = 'unix://var/run/docker.sock'
+def docker_client():
     if 'DOCKER_HOST' in os.environ:
-        docker_host = os.environ['DOCKER_HOST']
-    docker_api = docker.APIClient(base_url=docker_host)
-    return docker_api
+        return docker.from_env()
+    else:
+        docker_sock = 'unix://var/run/docker.sock'
+        return docker.DockerClient(base_url=docker_sock)
 
 
 @pytest.fixture()
-def image_meta_data(docker_api, testvars):
-    image_meta = docker_api.inspect_image(
+def image_meta_data(docker_client, testvars):
+    image_meta = docker_client.api.inspect_image(
         testvars['molecule_yml']['platforms'][0]['image'])
     return image_meta
